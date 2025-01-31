47c25fa4cc0aeead97964852505def11a4ee8296
Himanshu <2032ushimanshu@gmail.com>
2025-01-31 17:15:39 +0530

    PG

--- Array.py
+++ Array.py
@@ -1,4 +1,11 @@
+
+print(".....One Dimensional Array.....")
+
 from numpy import array 
 one_dimensional_array = array([1, 2, 3, 4, 5])
 print(one_dimensional_array)
 
+print(".....Two Dimensional Array.....")
+from numpy import array 
+two_dimensional_array = array([1, 2, 3], [4, 5, 6])
+print(two_dimensional_array)
